6bd232f5e787d19aa5d5f22ce383d13b5ffac448
Ian Logan <xeregin@gmail.com>
2014-06-25 21:14:12 +0000

    Commented out creation of flat network and vms

--- sat6-networks.sh
+++ sat6-networks.sh
@@ -29,15 +29,16 @@ cinder type-create Backup
 for i in `neutron security-group-list | awk '/default/ {print $2}'`; do neutron security-group-rule-create --protocol tcp --port-range-min 22 --port-range-max 22 --direction ingress $i; done
 for i in `neutron security-group-list | awk '/default/ {print $2}'`; do neutron security-group-rule-create --protocol icmp --direction ingress $i; done
 
-flatnetid=`neutron net-create ENV01-FLAT --provider:network_type=flat --provider:physical_network=ph-vmnet | grep -w id | awk '{print $4}'`
-neutron subnet-create ENV01-FLAT 10.127.100.0/24 --name ENV01-FLAT-SUBNET --no-gateway --host-route destination=0.0.0.0/0,nexthop=10.127.100.1 --allocation-pool start=10.127.100.128,end=10.127.100.159 --dns-nameservers list=true 8.8.8.8 8.8.4.4
+#flatnetid=`neutron net-create ENV01-FLAT --provider:network_type=flat --provider:physical_network=ph-vmnet | grep -w id | awk '{print $4}'`
+#neutron subnet-create ENV01-FLAT 10.127.100.0/24 --name ENV01-FLAT-SUBNET --no-gateway --host-route destination=0.0.0.0/0,nexthop=10.127.100.1 --allocation-pool start=10.127.100.128,end=10.127.100.159 --dns-nameservers list=true 8.8.8.8 8.8.4.4
 
-nova boot --flavor 2 --image precise-image --security-groups default --key-name key --nic net-id=$flatnetid flattest
+#nova boot --flavor 2 --image precise-image --security-groups default --key-name key --nic net-id=$flatnetid flattest
 
 vlan_provider() {
     vlannetid=`neutron net-create ENV01-VLAN --provider:network_type=vlan --provider:physical_network=ph-vmnet --provider:segmentation_id=867 | grep -w id | awk '{print $4}'`
     neutron subnet-create ENV01-VLAN 10.127.102.0/24 --name ENV01-VLAN-SUBNET --gateway 10.127.102.1 --allocation-pool start=10.127.102.128,end=10.127.102.159
     neutron net-update $vlannetid --router:external=true
+
     grenetid=`neutron net-create ENV01-GRE --provider:network_type=gre --provider:segmentation_id=1 | grep -w id | awk '{print $4}'`
     gresubid=`neutron subnet-create ENV01-GRE 192.168.105/24 --name ENV01-GRE-SUBNET --dns-nameservers list=true 8.8.8.8 8.8.4.4 | grep -w id | awk '{print $4}'`
 
